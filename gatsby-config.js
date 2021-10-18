f95b38a89613b9910107a03926c193fd2e5e528e
echo724 <eunchan1001@gmail.com>
2021-10-18 11:47:11 +0900

    Change G-tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
       resolve: `gatsby-plugin-gtag`,
       options: {
         // your google analytics tracking id
-        trackingId: `G-Y81TN9DWGG`,
+        trackingId: `G-6WSR15CRBP`,
         // Puts tracking script in the head instead of the body
         head: false,
         // enable ip anonymization
