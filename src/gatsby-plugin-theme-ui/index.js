72f1ae39094af021c5f19081afdf3bb394ae0d62
echo724 <eunchan1001@gmail.com>
2021-12-16 18:28:13 +0900

    Modify heading color

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -31,24 +31,49 @@ const theme = merge(originalTheme,{
             fontWeight: 400,
         },
         h1: {
-            color: `#383A3F`,
+            color: `text`,
             variant: `text.heading`,
             fontSize: [5, 6, 6, 6],
             mt: 4,
           },
         h2: {
-            color: `#383A3F`,
+            color: `text`,
             variant: `text.heading`,
             fontSize: [4, 5, 5, 5],
             mt: 4,
           },
         h3: {
-            color: `#383A3F`,
+            color: `text`,
             variant: `text.heading`,
             fontSize: [3, 4, 4, 4],
             mt: 4,
         },
     },
+    copyButton: {
+        backgroundColor: `transparent`,
+        border: `5px`,
+        color: `gray.2`,
+        cursor: `pointer`,
+        fontSize: [`14px`, `14px`, `16px`],
+        fontFamily: `body`,
+        letterSpacing: `0.025rem`,
+        transition: `default`,
+        "&[disabled]": {
+          cursor: `not-allowed`,
+          textDecorationLine: `underline`,
+        },
+        ":not([disabled]):hover": {
+          bg: `transparent`,
+          color: `white`,
+          textDecorationLine: `underline`,
+        },
+        position: `absolute`,
+        top: 0,
+        right: 0,
+        zIndex: 1,
+        borderRadius: `0 0 0 0.25rem`,
+        padding: `0.25rem 0.6rem`,
+      },
 });
 
 export default theme;
